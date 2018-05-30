361bffd37048803badaba5a8435d826d4f932efa
Kayla Reopelle <kreopelle@gmail.com>
2018-05-30 20:30:27 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ require 'pry'
 class Song
   extend Memorable
   extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
@@ -17,9 +18,9 @@ class Song
     @artist = artist
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+#  def to_param
+#    name.downcase.gsub(' ', '-')
+#  end
 
   def self.all
     @@songs
